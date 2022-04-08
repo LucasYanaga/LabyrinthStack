01f924f1da71de9b5a197b6af4e3912c27cbc935
Lucas <lucasyanaga@live.com>
2022-04-08 08:26:13 -0300

    Now the program accepts files with labyrinths but the file needs to be formatted

--- src/Labyrinth.java
+++ src/Labyrinth.java
@@ -1,19 +1,80 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
+
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class Labyrinth {
-    public String[][] laby = new String[11][11];
+    public String[][] laby;
     public Stack stack = new Stack(10);
-    public Coordinate entry = new Coordinate(1, 1);
-    public Coordinate exit = new Coordinate(1, 3);
+    public Coordinate entry;
+    public Coordinate exit;
     public Coordinate current;
-
-    public Labyrinth() {
+    public Labyrinth() throws IOException {
+        //this.createLaby();
+        this.readLaby();
         this.current = entry;
-        this.createLaby();
         this.stack.push(entry);
     }
 
+    public void readLaby() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("file3.txt"));
+        String line = br.readLine();
+
+        try {
+            StringBuilder sb = new StringBuilder();
+            int nCol = 0;
+            while (true) {
+                if(line == null){
+                    break;
+                }
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+
+                if(line != null){
+                    nCol = line.split("").length;
+                }
+            }
+
+            String all = sb.toString();
+            int nRow = all.split("\n").length;
+            String[][] labyAux= new String[nRow][nCol];
+
+            System.out.println(nRow);
+            System.out.println(nCol);
+
+            String[] lines = all.split(System.lineSeparator());
+
+            for (int i = 0; i < lines.length; i++) {
+                String[] items = lines[i].split("");
+                for (int j = 0; j < items.length; j++) {
+                    labyAux[i][j] = items[j];
+                }
+            }
+
+            this.laby = labyAux;
+
+            for (int i = 0; i < this.laby.length; i++) {
+                for (int j = 0; j < this.laby[i].length; j++) {
+                    if(Objects.equals(this.laby[i][j], "E")){
+                        entry = new Coordinate(i, j);
+                    }
+                    if(Objects.equals(this.laby[i][j], "S")){
+                        exit = new Coordinate(i, j);
+                    }
+                }
+            }
+
+        } finally {
+            br.close();
+        }
+    }
+
     public void createLaby(){
+        this.laby = new String[11][11];
         for (int i = 0; i < this.laby.length; i++) {
             for (int j = 0; j < this.laby.length; j++) {
 
@@ -60,13 +121,21 @@ public class Labyrinth {
         //Entrada e Saída
         this.laby[1][1] = "E";
         this.laby[1][3] = "S";
+
+        for (int i = 0; i < this.laby.length; i++) {
+            for (int j = 0; j < this.laby.length; j++) {
+                if(this.laby[i][j] == null){
+                    this.laby[i][j] = " ";
+                }
+            }
+        }
     }
 
     public void printLaby(){
         for (int i = 0; i < this.laby.length; i++) {
-            for (int j = 0; j < this.laby.length; j++) {
+            for (int j = 0; j < this.laby[i].length; j++) {
                 if(this.laby[i][j] == null){
-                    System.out.print(" ");
+                    //System.out.print(" ");
                 }else {
                     System.out.print(this.laby[i][j]);
                 }
@@ -84,13 +153,13 @@ public class Labyrinth {
         while(!this.gg(this.current)){
             move();
             this.printLaby();
-            MILLISECONDS.sleep(1000);
+            MILLISECONDS.sleep(250);
 
-            System.out.println(this.stack.peek().stringCoordinate() + " == " + this.exit.stringCoordinate());
+            //System.out.println(this.stack.peek().stringCoordinate() + " == " + this.exit.stringCoordinate());
         }
 
-        System.out.println("GG!");
         this.stack.printStack();
+        System.out.println("GG!");
     }
 
     public void move(){
@@ -99,22 +168,22 @@ public class Labyrinth {
         Coordinate left     = new Coordinate(this.current.x, this.current.y - 1);
         Coordinate right    = new Coordinate(this.current.x, this.current.y + 1);
 
-        if(this.laby[up.x][up.y] == null || this.laby[up.x][up.y].equals("S")){
+        if(Objects.equals(this.laby[up.x][up.y], " ") || this.laby[up.x][up.y].equals("S")){
             this.laby[up.x][up.y] = "-";
             this.current = up;
             this.stack.push(up);
             System.out.println("Go UP!");
-        }else if(this.laby[down.x][down.y] == null || this.laby[down.x][down.y].equals("S")){
+        }else if(Objects.equals(this.laby[down.x][down.y], " ") || this.laby[down.x][down.y].equals("S")){
             this.laby[down.x][down.y] = "-";
             this.current = down;
             this.stack.push(down);
             System.out.println("Go DOWN!");
-        }else if(this.laby[left.x][left.y] == null || this.laby[left.x][left.y].equals("S")){
+        }else if(Objects.equals(this.laby[left.x][left.y], " ") || this.laby[left.x][left.y].equals("S")){
             this.laby[left.x][left.y] = "-";
             this.current = left;
             this.stack.push(left);
             System.out.println("Go LEFT!");
-        }else if(this.laby[right.x][right.y] == null || this.laby[right.x][right.y].equals("S")){
+        }else if(Objects.equals(this.laby[right.x][right.y], " ") || this.laby[right.x][right.y].equals("S")){
             this.laby[right.x][right.y] = "-";
             this.current = right;
             this.stack.push(right);
@@ -126,8 +195,6 @@ public class Labyrinth {
             System.out.println("DEAD END!");
             System.out.println("Back to " + this.stack.peek().stringCoordinate());
         }
-
-        //this.laby[current.x][current.y] = "@";
     }
 
     public boolean gg(Coordinate cord){
